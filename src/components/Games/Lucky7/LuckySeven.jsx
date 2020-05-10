612e3b12b98b19805ecc53b2bb904102d587583f
Prashul Jain <jainprashul@gmail.com>
2020-05-10 21:09:32 +0530

    added Dice Animation

--- src/components/Games/Lucky7/LuckySeven.jsx
+++ src/components/Games/Lucky7/LuckySeven.jsx
@@ -1,26 +1,43 @@
 import React, { useState } from 'react'
-import { IonButton, IonGrid, IonRow, IonCol, IonSegment, IonSegmentButton } from '@ionic/react'
+import { IonButton, IonSegment, IonSegmentButton } from '@ionic/react'
+import ReactDice from 'react-dice-complete'
+import 'react-dice-complete/dist/react-dice-complete.css'
 import {getRandomInt} from './gameLogic'
 
-
+let Dice;
 const LuckySeven = () => {
 
+  const [rolling, setRolling] = useState(false);
+  const [rollingHistory, setRollingHistory] = useState([])
+  const [val, setVal] = useState(0);
+  const [bet, setBet] = useState(10);
+
+
   const roll = () => {
-    let x = getRandomInt(2, 12)
-    setVal(x)
+    setRolling(true);
+    // let x = getRandomInt(2, 12)
+    // setVal(x);
+    Dice.rollAll();
+
+    setTimeout(() => {
+      setRolling(false);
+    }, 2000)
   }
+
+  const rollDone = (num) => {
+    setVal(num);
+    setRollingHistory([...rollingHistory , num])
+
+  }
+
 
   const bettingSet = (val) => {
     if ((bet <= 10) && (val < 0)) {
-      
+
     } else {
       setBet(bet + val);
     }
   }
-
-
-  const [val, setVal] = useState(0)
-  const [bet, setBet] = useState(10);
   return (
     <div className='ion-text-center'>
 
@@ -28,8 +45,9 @@ const LuckySeven = () => {
         Select Option
       </h4>
 
-      <IonSegment>
-        <IonSegmentButton value='<7'>
+      <IonSegment onIonChange={e => console.log(e)
+      }>
+        <IonSegmentButton value='lessThan7'>
           <h2>2 To 6</h2>
           <p>1.5 x amt</p>
           Jeeto &#8377;{bet * 1.5}
@@ -40,7 +58,7 @@ const LuckySeven = () => {
           <p>Jeeto &#8377;{bet * 2}</p>
 
         </IonSegmentButton>
-        <IonSegmentButton value='>7'>
+        <IonSegmentButton value='greaterThan7'>
           <h2>8 To 12</h2>
           <p>1.5 x amt</p>
           Jeeto &#8377;{bet * 1.5}
@@ -53,11 +71,22 @@ const LuckySeven = () => {
 
       <p>
         <IonButton size='small' shape='round' onClick={()=> bettingSet(-10)}><strong>-</strong></IonButton>
-        <span style={{ fontSize: '40px' }}>&#8377; {bet} </span>
+          <span style={{ fontSize: '40px' }}>  &#8377; {bet} </span>
         <IonButton size='small' shape='round' onClick={() => bettingSet(10)}><strong>+</strong></IonButton>
       </p>
 
-      <IonButton size='large' shape='round' onClick={roll}>Roll</IonButton>
+      
+
+      <ReactDice
+        numDice={2}
+        rollDone={rollDone}
+        disableIndividual={true}
+        faceColor={'#e1e1e1'}
+        dotColor={'#0e0e0e'}
+        ref={(dice) => Dice = dice}
+      />
+      <IonButton disabled={rolling} size='large' shape='round' onClick={roll}>Roll</IonButton>
+
     </div>
   )
 }
