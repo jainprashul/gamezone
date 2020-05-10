350f72d811586e6fb18313c85c8923bff667a0bb
Prashul Jain <jainprashul@gmail.com>
2020-05-10 22:51:59 +0530

    Game Done without balance Integration

--- src/components/Games/Lucky7/LuckySeven.jsx
+++ src/components/Games/Lucky7/LuckySeven.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { IonButton, IonSegment, IonSegmentButton } from '@ionic/react'
 import ReactDice from 'react-dice-complete'
 import 'react-dice-complete/dist/react-dice-complete.css'
+import { toastController, alertController} from '@ionic/core'
 import {getRandomInt} from './gameLogic'
 
 let Dice;
@@ -9,11 +10,22 @@ const LuckySeven = () => {
 
   const [rolling, setRolling] = useState(false);
   const [rollingHistory, setRollingHistory] = useState([])
+  const [betOption, setBetOption] = useState(null);
   const [val, setVal] = useState(0);
   const [bet, setBet] = useState(10);
 
 
   const roll = () => {
+    if (betOption == null) {
+      toastController.create({
+        color: 'warning',
+        duration: 1000,
+        message: "Select Bet Option First",
+        position: "top",
+
+      }).then(res => res.present());
+      return;
+    }
     setRolling(true);
     // let x = getRandomInt(2, 12)
     // setVal(x);
@@ -26,14 +38,52 @@ const LuckySeven = () => {
 
   const rollDone = (num) => {
     setVal(num);
-    setRollingHistory([...rollingHistory , num])
-
+    setRollingHistory([...rollingHistory, num])
+    checkBetting(num);
   }
 
+  const alertCall= (bool, betMutiplier)=> {
+    if (bool) {
+      alertController.create({
+        header: 'Yipee You Won !',
+        backdropDismiss: false,
+        message: ` Win Rs. ${bet * betMutiplier}`,
+        buttons: ['OK']
+      }).then(t => t.present());
+    } else {
+      alertController.create({
+        header: 'You Lose !',
+        message: ` Loses Rs. ${bet }`,
+        buttons: ['OK']
+      }).then(t => t.present());
+    }
+  }
+
+  const checkBetting = (value) => {
+    switch (betOption) {
+      case 'lessThan7':
+        alertCall(value < 7, 1.5);
+        break;
+      case '7':
+        alertCall(value === 7, 2);
+        break;
+      case 'greaterThan7':
+        alertCall(value > 7, 1.5);
+        break;
+      
+    
+      default:
+        toastController.create({
+          color: 'warning',
+          duration: 1000,
+          message: "Select Betting Option First"
+        })
+        return false;
+    }
+  }
 
   const bettingSet = (val) => {
     if ((bet <= 10) && (val < 0)) {
-
     } else {
       setBet(bet + val);
     }
@@ -45,8 +95,7 @@ const LuckySeven = () => {
         Select Option
       </h4>
 
-      <IonSegment onIonChange={e => console.log(e)
-      }>
+      <IonSegment onIonChange={e => setBetOption(e.detail.value)}>
         <IonSegmentButton value='lessThan7'>
           <h2>2 To 6</h2>
           <p>1.5 x amt</p>
